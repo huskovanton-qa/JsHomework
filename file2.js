a4be02a0d4fb9950694af3d3e5e9bc0ac7a5a129
Huskov Anton <anton.huskov@appflame.com>
2025-12-19 12:11:19 +0200

    Huskov HW

--- file2.js
+++
@@ -1,8 +0,0 @@
-const regViraz = /[^Aa]{6,}/;
-
-console.log(regViraz.test("Wonderful"));
-console.log(regViraz.test("Joyful")); 
-console.log(regViraz.test("Happiness")); 
-console.log(regViraz.test("Time")); 
-console.log(regViraz.test("Task")); 
-console.log(regViraz.test("Apple")); 
